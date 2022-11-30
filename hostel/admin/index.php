dc612ab40bd2e3d730b7f55e15fd399089db6810
Shreyash Shriram Patil <89066349+Shreyashpatil056@users.noreply.github.com>
2022-11-30 12:32:40 +0530

    updated

--- hostel/admin/index.php
+++ hostel/admin/index.php
@@ -56,7 +56,7 @@ $stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (user
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 col-md-offset-3" style="margin-top:4%" >
-						<h1 class="text-center text-bold text-white mt-4x"   >SGGS Hostel Management System</h1>
+						<h1 class="text-center text-bold text-white mt-4x"   style="background-color:#494949; padding:5px; font-size:30px; border-radius:8px ">SGGS Hostel Management System</h1>
 						<div class="well row pt-2x pb-3x bk-light" style="background-color: transparent; " >
 							<div class="col-md-8 col-md-offset-2" >
 							
@@ -65,7 +65,26 @@ $stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (user
 									<input type="text" placeholder="Username" name="username" class="form-control mb">
 									<label for="" class="text-uppercase text-sm"style="color: white "  >Password</label>
 									<input type="password" placeholder="Password" name="password" class="form-control mb">
-									<input type="submit" name="login" class="btn btn-success btn-block" value="login" >
+<!-- captcha code-------------------------------------------------------------------------  -->
+									<label for="" style="color: white ">Enter Captcha</label>
+                                            <div class="form-row">
+                                                <div class="form-group col-md-6">
+                                                    <input type="text" class="form-control" readonly id="capt">
+                                                </div>
+                                                <!-- <p>huhhhhh</p> -->
+                                                <div class="form-group col-md-6">
+                                                    <input type="text" class="form-control" id="textinput">
+                                                </div>
+                                            </div>
+
+											
+
+                                            <input type="submit" onclick="validcap()" name="login"
+                                                class="btn btn-primary btn-block" value="login">
+
+												<h5 style="color: white ">Captcha not visibleE <img src="./img/refresh-16.png" onclick="cap()"></h5>
+<!-- --------------------------------------------------------------------------------------  -->
+									<!-- <input type="submit" name="login" onclick="validcap()" class="btn btn-success btn-block" value="login" > -->
 								</form>
 							</div>
 						</div>
@@ -74,6 +93,40 @@ $stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (user
 			</div>
 		</div>
 	</div>
+	<script type="text/javascript">
+    function cap() {
+        var alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e',
+            'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '#', '$',
+            '%', '^', '&', '*', '+'
+        ];
+        var a = alpha[Math.floor(Math.random() * 71)];
+        var b = alpha[Math.floor(Math.random() * 71)];
+        var c = alpha[Math.floor(Math.random() * 71)];
+        var d = alpha[Math.floor(Math.random() * 71)];
+        var e = alpha[Math.floor(Math.random() * 71)];
+        var f = alpha[Math.floor(Math.random() * 71)];
+
+        var final = a + b + c + d + e + f;
+        document.getElementById("capt").value = final;
+    }
+
+    function validcap() {
+        var stg1 = document.getElementById('capt').value;
+        var stg2 = document.getElementById('textinput').value;
+        if (stg1 == stg2) {
+            alert("Form is validated Succesfully");
+            return true;
+        } else {
+            alert("Please enter a valid captcha");
+			location.reload();
+			history.go(-1)
+            return false;
+        }
+    }
+    </script>
+
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap-select.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
